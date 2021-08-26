dbebfdf93411d7d6f267a2f79f0fd30c2ba2f7ce
Hladikes <adi.hladik@gmail.com>
2021-08-26 20:58:29 +0200

    feature: regex for dynamic class extracting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./src/**/*.svelte'],
+  purge: {
+    content: ['./public/index.html', './src/**/*.svelte'],
+    options: {
+      defaultExtractor: content => [
+        ...(content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []),
+        ...(content.match(/(?<=class:)[^=>\/\s]*/g) || []),
+        ...(content.match(/([a-zA-Z0-9\-]{1,})=/g) || []).map(s=>s.substr(0,s.length-1))
+      ],
+    }
+  },
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
